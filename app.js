b54d7d4c189ee1b4440338ba08a88f3834e21c98
Dominik Skutecki <150598081+Dominik9123@users.noreply.github.com>
2025-02-05 01:33:08 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -36,3 +36,15 @@ function closeAllSubMenus() {
 })
 }
 
+window.onscroll = function () {
+    let button = document.getElementById("backToTop");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
